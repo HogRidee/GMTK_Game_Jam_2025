71be16a65a1d168c408baafaff1bc044c5c702e1
Iván Alexander Aráoz Andrade <iaraoz061102@gmail.com>
2025-07-31 16:27:20 -0500

    Add ResourceBarTracking script for UI resource bars
    Introduces a new ResourceBarTracking MonoBehaviour to manage and update UI resource bars in the game. The script handles resource value changes, bar fill updates, and supports an overkill option for resource depletion.

---
+++ Assets/Scripts/UI/Game/ResourceBarTracking.cs
@@ -0,0 +1,41 @@
+using Unity.Mathematics.Geometry;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceBarTracking : MonoBehaviour
+{
+    [Header("Core Settings")]
+    [SerializeField] private Image bar;
+    [SerializeField] private int resourceCurrent = 100;
+    [SerializeField] private int resourceMax = 100;
+    [Space]
+    [SerializeField] private bool overkillPossible;
+
+    private void Start()
+    {
+        UpdateBarAndResourceText();
+    }
+    
+    private void UpdateBarAndResourceText()
+    {
+        if (resourceMax <= 0)
+        {
+            bar.fillAmount = 0;
+            return;
+        }
+        float fillAmount = (float) resourceCurrent / resourceMax;
+        bar.fillAmount = fillAmount;
+    }
+
+    public bool ChangeResourceByAmount(int amount)
+    {
+        if (!overkillPossible && resourceCurrent + amount < 0)
+        {
+            return false;
+        }
+        resourceCurrent += amount;
+        resourceCurrent = Mathf.Clamp(resourceCurrent, 0, resourceMax);
+        bar.fillAmount = (float) resourceCurrent / resourceMax;
+        return true;
+    }
+}
