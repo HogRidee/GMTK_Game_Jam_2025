66c895cbba4fb63c85250d339a141caa7fde7c19
Iván Alexander Aráoz Andrade <iaraoz061102@gmail.com>
2025-08-01 19:51:01 -0500

    Add Spawn UI prefab, test scene, and SpawnManager
    Introduced a new 'Spawn Icon' UI prefab and its meta files, created a test scene 'SpawnBar Test.unity' for UI testing, and added the initial implementation of the SpawnManager script for managing spawn-related UI logic.

---
+++ Assets/Scripts/UI/Game/Spawn/SpawnManager.cs
@@ -0,0 +1,72 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum SpawnType { Thief, Civilian, Mayor }
+
+public class SpawnData
+{
+    public SpawnType    Type;
+    public float        SpawnTime;
+    public RectTransform IconRt;
+}
+
+public class SpawnManager : MonoBehaviour
+{
+    [Header("UI References")]
+    [SerializeField] private RectTransform spawnBarRt;
+    [SerializeField] private GameObject    spawnIconPrefab;
+
+    [Header("Lead Time")]
+    [SerializeField] private float leadTime = 5f;
+
+    [Header("Spawner")]
+    // [SerializeField] private NPCSpawner npcSpawner;
+
+    private List<SpawnData> queue = new List<SpawnData>();
+
+    void Update()
+    {
+        float now   = Time.time;
+        float halfW = spawnBarRt.rect.width * 0.5f;
+        for (int i = queue.Count - 1; i >= 0; i--)
+        {
+            var sd   = queue[i];
+            float tRem = sd.SpawnTime - now;
+
+            if (tRem <= 0f)
+            {
+                // npcSpawner.RequestSpawn(sd.Type);
+                Destroy(sd.IconRt.gameObject);
+                queue.RemoveAt(i);
+            }
+            else
+            {
+                float norm = Mathf.Clamp01(tRem / leadTime);
+                float x     = Mathf.Lerp(-halfW, +halfW, norm);
+                sd.IconRt.anchoredPosition = new Vector2(x, 0);
+            }
+        }
+    }
+    
+    public void ScheduleSpawn(SpawnType type, float delay)
+    {
+        float tSpawn = Time.time + delay;
+        var iconGO   = Instantiate(spawnIconPrefab, spawnBarRt);
+        var iconRt   = iconGO.GetComponent<RectTransform>();
+        float halfW = spawnBarRt.rect.width * 0.5f;
+        iconRt.anchoredPosition = new Vector2(+halfW, 0);
+        var img = iconGO.GetComponent<Image>();
+        img.sprite = GetSpriteForType(type);
+        queue.Add(new SpawnData {
+            Type      = type,
+            SpawnTime = tSpawn,
+            IconRt    = iconRt
+        });
+    }
+
+    Sprite GetSpriteForType(SpawnType type)
+    {
+        return null;
+    }
+}
